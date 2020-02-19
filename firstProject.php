f071a69b66974a2ad4c302af03f4ce97e989c283
Tim Spengler <31554315+timothyspengler@users.noreply.github.com>
2020-02-19 13:00:53 -0800

    Update firstProject.php

--- firstProject.php
+++ firstProject.php
@@ -5,5 +5,5 @@
     header("Content-Type: application/json");
     // build a PHP variable from JSON sent using POST method
     $v = json_decode(file_get_contents("php://input"));
-    echo "Employee Accepted. Hi from php";
+    echo "Total Employees";
 ?>
