b6c2baf07cb236d3ecc0d743546f49dbebd9280f
timothyspengler <timothy.spengler.571@my.csun.edu>
2020-02-18 22:55:32 -0800

    adding files

---
+++ firstProject.php
@@ -0,0 +1,9 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+    // header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With, Content-Type");
+    header("Content-Type: application/json");
+    // build a PHP variable from JSON sent using POST method
+    $v = json_decode(file_get_contents("php://input"));
+    echo "Employee Accepted. Hi from php";
+?>
